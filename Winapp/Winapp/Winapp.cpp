f5ff57d7913f41ddaeb3907c4390a831c9290937
JS Kim <wotjr4836@naver.com>
2021-11-15 20:13:39 +0900

    no message

---
+++ Winapp/Winapp/Winapp.cpp
@@ -0,0 +1,62 @@
+#define _CRT_SECUER_NO_WARNINGS // (LNK2019, LNK1120) 오류시 프로젝트 속성 -> 링커 -> 시스템 -> 하위시스템 -> 콘솔에서 창으로 변경
+#include <windows.h>
+#include <stdio.h>
+
+// 윈도우 프로시저
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+// 편집 컨트롤 출력 함수
+void DisplayText(char* fmt, ...);
+
+HINSTANCE hlnst; //인스턴스 핸들
+HWND hEdit; // 편집 컨트롤
+
+int WINAPI WinMain(HINSTANCE hlnstance, HINSTANCE hPrevInstance,
+	LPSTR IpCmdLine, int nCmdShow)
+{	
+	hlnst = hlnstance;
+
+	// 윈도우 클래스 등록
+	WNDCLASS wndclass;
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hlnstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = "MyWndClass";
+	if (!RegisterClass(&wndclass)) return 1;
+
+	// 윈도우 생성
+	HWND hWnd = CreateWindow("MyWndClass", "WinApp", WS_OVERLAPPEDWINDOW,
+		0, 0, 600, 200, NULL, NULL, hlnstance, NULL);
+	if (hWnd == NULL) return 1;
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	// 메시지 루프
+	MSG msg;
+	while (GetMessage(&msg, 0, 0, 0) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+//윈도우 프로시저
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM IParam)
+{
+	switch (uMsg) {
+	case WM_CREATE:
+		return 0;
+	case WM_SIZE:
+		return 0;
+	case WM_SETFOCUS:
+		return 0;
+	case WM_DESTROY:
+		return 0;
+	}
+	return DefWindowProc(hWnd, uMsg, wParam, IParam);
+}
